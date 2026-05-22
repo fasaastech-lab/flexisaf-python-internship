7c34b1a58300278842f3581240d6223b53ee74d5
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-05-22 21:46:37 +0100

    Add input validation for age, phone, and email

--- week1_deliverables.py
+++ week1_deliverables.py
@@ -1,8 +1,28 @@
 name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-email = input("Enter your email: ")
-phone = input("Enter your phone no: \n")
-
+#Validating age
+while True:
+    try:
+        age = int(input("Enter your age: "))
+    except ValueError:
+        print("Age is not an integer")
+    else:
+        break
+#Validating phone_number
+while True:    
+    try:
+        phone = int(input("Enter your phone: "))
+    except ValueError:
+        print("Phone is not an number")
+    else:
+        break
+#Validating email
+while True:
+    email = input("Enter your email: ")
+    if "@" or '.' not in email:
+        print('Invalid Email')
+    else:
+        break
+#Displaying user's profile
 print("USER PROFILE:")
 print(f"Name: {name}")
 print(f"Age: {age}")
