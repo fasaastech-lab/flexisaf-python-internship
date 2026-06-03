aff3ea00b499193c9237124ac04c6c351ed7a87e
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-06-03 15:54:15 +0100

    Fix age validator and login flow code

--- week3_deliverables.py
+++ week3_deliverables.py
@@ -1,19 +1,20 @@
-# # Categorise user by age group
-# age = int(input("Enter your age: "))
-# if age < 13:
-#     print("You are a child")
-# elif age < 20:
-#     print("You are a teenager")
-# elif age < 65:
-#     print("You are an adult")
-# else:
-#     print("You are a senior")
-
-# 
+# Categorise user by age group
+age = int(input("Enter your age: "))
+if age < 13:
+    print("You are a child")
+elif age < 20:
+    print("You are a teenager")
+elif age < 65:
+    print("You are an adult")
+else:
+    print("You are a senior")
+# Define credentials
 username = "fastmode"
-password = 12345
+password = "12345"
+# Collect user login input
 user_name = input("Enter your username: ")
-pass_word = int(input("Enter your password: "))
+pass_word = input("Enter your password: ")
+# Validate credentials
 if username == user_name and password == pass_word:
     print("Access granted")
 else:
