b7c8bdddc38d71fe0962540cb74746137aad6e2f
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-06-03 15:44:59 +0100

    Implement login flow with credential check

---
+++ week3_deliverables.py
@@ -0,0 +1,20 @@
+# # Categorise user by age group
+# age = int(input("Enter your age: "))
+# if age < 13:
+#     print("You are a child")
+# elif age < 20:
+#     print("You are a teenager")
+# elif age < 65:
+#     print("You are an adult")
+# else:
+#     print("You are a senior")
+
+# 
+username = "fastmode"
+password = 12345
+user_name = input("Enter your username: ")
+pass_word = int(input("Enter your password: "))
+if username == user_name and password == pass_word:
+    print("Access granted")
+else:
+    print("Access Denied")
