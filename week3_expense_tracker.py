c90c71543bfbd48e863afb05fd667c37a479a235
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-06-04 14:32:57 +0100

    Implement budget overspending check

--- week3_expense_tracker.py
+++ week3_expense_tracker.py
@@ -1,4 +1,13 @@
-monthly_budget = float(input("Enter your monthly budget"))
-food = float(input("Enter your food expenses"))
-transport = float(input("Enter your transport expenses"))
-utilities = float(input("Enter your utilities expenses"))
+# Collect monthly budget and expense inputs
+monthly_budget = float(input("Enter your monthly budget: "))
+food = float(input("Enter your food expenses: "))
+transport = float(input("Enter your transport expenses: "))
+utilities = float(input("Enter your utilities expenses: "))
+# Sum expenses
+total_expenses = food + transport + utilities
+print(f"Total expenses: {total_expenses}")
+# Comparing budget and expenses
+if total_expenses <= monthly_budget:
+    print("Your expenses are within budget")
+else: 
+    print("You're spending above your budget")
