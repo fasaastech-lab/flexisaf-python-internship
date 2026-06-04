a9bf27795b61fead9c2dad39fb7a09289f67123a
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-06-04 14:54:25 +0100

    Refactor expense tracker to show balance

--- week3_expense_tracker.py
+++ week3_expense_tracker.py
@@ -3,11 +3,13 @@ monthly_budget = float(input("Enter your monthly budget: "))
 food = float(input("Enter your food expenses: "))
 transport = float(input("Enter your transport expenses: "))
 utilities = float(input("Enter your utilities expenses: "))
-# Sum expenses
+# sum expenses
 total_expenses = food + transport + utilities
 print(f"Total expenses: {total_expenses}")
-# Comparing budget and expenses
-if total_expenses <= monthly_budget:
+balance = monthly_budget - total_expenses
+print(f"Balance: {balance}")
+# Compare expense and budget
+if balance >= 0:
     print("Your expenses are within budget")
 else: 
     print("You're spending above your budget")
