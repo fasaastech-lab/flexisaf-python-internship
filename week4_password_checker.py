57c498bf45f43d7b4477c51162818f3f81d597e8
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-06-12 11:52:55 +0100

    Implement password characters scan loop

--- week4_password_checker.py
+++ week4_password_checker.py
@@ -9,3 +9,14 @@ elif len(password) >= 8:
 else:
     print("Your password length is weak")
 
+# Set flag for numbers and special characters in password
+has_number = False
+has_special = False
+special = ['!','@','#','$','%','&','*','~','|','/']
+
+# Verify number and special characters in password
+for char in password:
+    if char.isdigit():
+        has_number = True
+    if char in special:
+        has_special = True
