e654bca1bf7698228a85fc1353618efd6a62b6ea
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-06-30 15:40:59 +0100

    Add menu skeleton for student contact manager

---
+++ week6_deliverables.py
@@ -0,0 +1,25 @@
+contacts = {}
+used_emails = set()
+used_phones = set()
+
+while True:
+    print("\n1. Add contact")
+    print("2. View contacts")
+    print("3. Search contact")
+    print("4. Update contact")
+    print("5. Delete contact")
+    print("6. Exit")
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        pass
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        pass
+    elif choice == "4":
+        pass
+    elif choice == "5":
+        pass
+    elif choice == "6":
+        break
