3316f568434c7fef3e1d266b72bbb30ee01bc6ce
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-07-08 17:57:09 +0100

    Implement todo app logic

---
+++ week7_todo_app.py
@@ -0,0 +1,29 @@
+from week7_todo_functions import add_task, view_task, mark_done, delete_task
+
+tasks = []
+
+# Main loop
+while True:
+    print("\n1. Add task")
+    print("2. View tasks")
+    print("3. Mark task as done")
+    print("4. Delete task")
+    print("5. Exit")
+    option = input("Choose an option: ")
+
+    if option == "1":
+        task = input("Enter task: ")
+        add_task(tasks, task)
+    elif option == "2":
+        view_task(tasks)
+    elif option == "3":
+        num = int(input("Enter task number: "))
+        mark_done(tasks, num)
+    elif option == "4":
+        num = int(input("Enter task number: "))
+        delete_task(tasks, num)
+    elif option == "5":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid input. Choose a number between 1 and 5")
