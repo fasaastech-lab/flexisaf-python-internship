bde557343844ea12c95daadea183976fa72f2be3
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-07-08 09:53:47 +0100

    Add all task functions for CLI to-do list app

--- week7_todo_functions.py
+++ week7_todo_functions.py
@@ -5,7 +5,7 @@ def add_task(tasks, task):
 
 def view_task(tasks):
     for i, task in enumerate(tasks):
-        print(f"{i+1}. {task[0]} - {task[0][1]}")
+        print(f"{i+1}. {task[0]} - {task[1]}")
 
 
 def mark_done(tasks, num):
