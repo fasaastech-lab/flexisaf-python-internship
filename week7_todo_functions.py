d99f39526614f9a76b538707d75547a9a97ee0fd
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-07-08 09:40:13 +0100

    Update task functions for CLI to-do list app

--- week7_todo_functions.py
+++ week7_todo_functions.py
@@ -5,8 +5,10 @@ def add_task(tasks, task):
 
 def view_task(tasks):
     for i, task in enumerate(tasks):
-        print(f"{i+1}. {task}")
+        print(f"{i+1}. {task[0]} - {task[0][1]}")
 
 
-
+def mark_done(tasks, num):
+    tasks[num-1][1] = "Done"
+    print("Task is done")
     
