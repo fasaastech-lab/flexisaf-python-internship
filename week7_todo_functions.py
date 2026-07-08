e0565d7465b1f010b6ec96eb9ecacec305407c77
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-07-08 18:32:59 +0100

    Update functions to handle invalid input

--- week7_todo_functions.py
+++ week7_todo_functions.py
@@ -1,14 +1,33 @@
+# CLI To-Do List App fuctions
+
 def add_task(tasks, task):
+    # Add tasks
     tasks.append([task, "Pending"])
     print("Task was added")
 
 
 def view_task(tasks):
-    for i, task in enumerate(tasks):
-        print(f"{i+1}. {task[0]} - {task[1]}")
+    # Display tasks
+    if not tasks:
+        print("No added task yet")
+    else:
+        for i, task in enumerate(tasks):
+            print(f"{i+1}. {task[0]} - {task[1]}")
 
 
 def mark_done(tasks, num):
-    tasks[num-1][1] = "Done"
-    print("Task is done")
-    
+    # Mark tasks as done
+    if 1 > num > len(tasks):
+        print("Invalid task number")
+    else: 
+        tasks[num-1][1] = "Done"
+        print("Task is done")
+        
+
+def delete_task(tasks, num):
+    # Delete tasks
+    if 1 <= num <= len(tasks):
+        tasks.pop(num-1)
+        print("Tasks deleted")
+    else:
+        print("Invalid task number")
