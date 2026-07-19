a67bfb91ab8a8a92f1fe584642ef02678d487ad5
Abdulraheem Fasasi <fasaastech@gmail.com>
2026-07-19 18:01:31 +0100

    Fix math tools module with correct function definitions

---
+++ week10_math_tools.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a-b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        print("Zero can't be a divisor")
+    else:
+        return a/b
